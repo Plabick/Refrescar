2f7408834c54548ee304cd64135a886a05c9c4be
Peter Labick <Plabick@users.noreply.github.com>
2021-10-12 23:06:06 -0400

    Plabick/prepare release (#1)
    * Create readme
    
    * Add draft 1
    
    * Update README.md
    
    * Finish version 0.1

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Refrescar
-Easily enable the Windows 11 update on PCs without supported CPUs
+Easily enable the Windows 11 update on PCs without TPM 2.0 chips or officaly supported CPUs.
+
+Why is this needed? Microsoft artificaly blocks Windows 11 from being installed on computers with AMD Ryzen first generation CPUs and computers with TPM 1.2 chips  despite the fact that Windows 11 can run well on these "incompatible" systems. Refrescar bypasses the CPU and TPM check during Windows 11 installation by safely modifying the Windows Registry.
+
+**Refrescar works on computers running Windows 10 that are blocked from updating due to incompatible CPUs and TPM 1.2 only. Refrescar does not work on computers that are blocked from updating because they lack UEFI boot or because the contain no TPM module. Use [Microsoft's PC Health Check](https://aka.ms/GetPCHealthCheckApp) to see why your PC is blocked from updating.**
+## Usage
+
+#### Download and run Refrescar
+TODO
+
+#### Whats next?
+After running Refrescar, your PC will be able to update to Windows 11 - you'll still need to install it. 
+
+1. Download the [Windows 11 Installation Assistant](https://www.microsoft.com/en-us/software-download/windows11) directly from Microsoft
+
+2. Open the Assistant and select `Download Windows 11 Disk Image (ISO)`. Go through the prompts and download the ISO. 
+
+3. When the ISO finishes downloading, right-click on it and pick `Open with`, then `Windows Explorer` to mount the virtual disc.  Then double-click the setup file to begin installation.
+
+4. Thats it! Just be aware that Microsoft reserves the right to withhold future security updates if you install Windows 11 on an unsupported machine. 
+ 
+## Contributing
+1. Fork this repository.
+2. Create a branch: `git checkout -b <branch_name>`.
+3. Make your changes and commit them: `git commit -m '<commit_message>'`
+4. Push to the original branch: `git push origin <project_name>/<location>`
+5. Create the pull request.
+
+Alternatively see the GitHub documentation on [creating a pull request](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/creating-a-pull-request).
---
+++ main.py
@@ -0,0 +1,85 @@
+from tkinter import *
+from tkinter import messagebox
+
+from PIL import ImageTk, Image
+from winreg import *
+
+if __name__ == '__main__':
+    REGISTRY_LOCATION = r'SYSTEM\Setup\MoSetup'
+    REGISTRY_KEY = r'AllowUpgradesWithUnsupportedTPMOrCPU'
+
+
+    def on_click():
+        overwrite_window()
+        update_registry()
+
+
+    def update_registry():
+        if not bypass_enabled:
+            # Create key
+            key = CreateKey(HKEY_LOCAL_MACHINE, REGISTRY_LOCATION)
+            SetValueEx(key, REGISTRY_KEY, 0, REG_DWORD, 1)
+            CloseKey(key)
+
+
+    def overwrite_window():
+        for widget in window.winfo_children():
+            widget.destroy()
+        success = Label(text="Success", anchor=CENTER, bg="white", font=('Arial bold', '20'))
+        enabled = Label(text="The Windows 11 update has been enabled", anchor=CENTER, bg="white",
+                        font=('Arial ', '15'))
+        next = Label(text="Use the Windows 11 Upgrade Assistant to install the update", anchor=CENTER, bg="white",
+                        font=('Arial ', '12'))
+        success.pack()
+        enabled.pack()
+        next.pack()
+
+
+    try:
+        # Check for access to Windows Registry
+        fullKey = OpenKey(HKEY_LOCAL_MACHINE, REGISTRY_LOCATION, 0, KEY_ALL_ACCESS)
+    except PermissionError:
+        messagebox.showinfo('Error', 'Please run Refrescar as an Administrator')
+        exit(1)
+
+    try:
+        # Check if key exists and is set to the correct value
+        current = QueryValueEx(fullKey, REGISTRY_KEY)
+        bypass_enabled = current[0] == 1
+    except FileNotFoundError:
+        bypass_enabled = False;
+
+    # Create Window
+    window = Tk()
+    window.title("Refrescar")
+    window.geometry('500x300')
+    window.configure(bg="white")
+
+    # Add logo to window
+    logo = Image.open("images/Refrescar.ppm").resize((318, 105))
+    logoImage = ImageTk.PhotoImage(logo)
+    logoLabel = Label(image=logoImage, bg="white")
+    logoLabel.image = logoImage
+    logoLabel.pack()
+
+    # Display Current Patch Status
+    if bypass_enabled:
+        status_message = "Patched"
+        status_color = "green"
+    else:
+        status_message = "Unpatched"
+        status_color = "red"
+    status = Label(text=f"Current Status: {status_message}", anchor=CENTER, fg=status_color, bg="white")
+    status.place(relx=0.5, rely=0.8, anchor=CENTER)
+
+    # Display tag line
+    tagLine = Label(text="Enable Windows 11 installation on computers with unsupported CPUs", anchor=CENTER, bg="white")
+    tagLine.place(relx=0.5, rely=0.9, anchor=CENTER)
+
+    # Display Run button
+    button = Image.open("images/button.pbm").resize((275, 42))
+    button_image = ImageTk.PhotoImage(button)
+    btn = Button(window, image=button_image, bg='white', fg='white', highlightthickness=0, bd=0, command=on_click)
+    btn.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+    window.mainloop()
